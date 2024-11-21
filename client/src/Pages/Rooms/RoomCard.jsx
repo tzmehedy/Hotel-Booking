dc8ed9d4e3259f28af0d3375d25e35d428113e23
unknown <mehedytouhidurzaman@gmail.com>
2024-11-21 14:33:25 +0600

    room page desing and show data from database is completed

---
+++ client/src/Pages/Rooms/RoomCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const RoomCard = ({room}) => {
+    
+    return (
+      <div>
+        <div className="card card-compact bg-base-100 max-w-96 h-96 shadow-xl">
+          <figure>
+            <img src={room.roomImages} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{room.category}</h2>
+            <p>{room.roomDescription.substring(0, 70)}...</p>
+            <p className="font-bold">Price: ${room.pricePerNight} /per-night</p>
+            <p className="text-[#f99810f6] font-bold">
+              Available: {room.availability ? "Yes" : "No"}
+            </p>
+            <p className="text-[#f99810f6] font-bold">
+              Offer:{" "}
+              {room.specialOffers
+                ? room.specialOffers
+                : "No Offer is available at this moment"}
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-[#f99810f6]">Book Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default RoomCard;
--- client/src/Pages/Rooms/Rooms.jsx
+++ client/src/Pages/Rooms/Rooms.jsx
@@ -1,10 +1,28 @@
-
+import { useEffect, useState } from "react";
+import axios from "axios"
+import RoomCard from "./RoomCard";
 
 const Rooms = () => {
+    const [rooms, setRooms] = useState([])
+
+    const [categories, setcategories] = useState([])
+
+    useEffect(()=>{
+        getData();
+    }, [])
+
+    const getData = async() =>{
+        const { data } = await axios.get(`${import.meta.env.VITE_Url}/rooms`)
+        setRooms(data)
+    }
+
+    
     return (
-        <div className="mt-20">
-            <h1>This is room page</h1>
-        </div>
+      <div className="mt-20 p-5 grid grid-cols-1 md:grid-cols-3 gap-10">
+        {rooms.map((room) => (
+          <RoomCard room={room}></RoomCard>
+        ))}
+      </div>
     );
 };
 
