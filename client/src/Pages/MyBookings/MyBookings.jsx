04529d25d50d8b48c3b707d177a895960bff124c
unknown <mehedytouhidurzaman@gmail.com>
2024-11-22 11:06:51 +0600

    data show in a tabel in my bookings page is completed

--- client/src/Pages/MyBookings/MyBookings.jsx
+++ client/src/Pages/MyBookings/MyBookings.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import MyBookingsTable from './MyBookingsTable';
 
 const MyBookings = () => {
+    const {user} = useContext(AuthContext)
+    const [bookedInfo, setBookedInfo] = useState([])
+
+    useEffect(()=>{
+        const getBookedInfo = async() =>{
+            const { data } = await axios.get(
+              `http://localhost:5000/myBookedInfo?email=${user?.email}`
+            )
+            setBookedInfo(data)
+        }
+        getBookedInfo()
+    },[])
+
+
+    
     return (
-        <div className='mt-20'>
-            <h1>This is my bookings page</h1>
+      <div className="mt-20">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Email</th>
+                <th>Check In</th>
+                <th>Check Out</th>
+                <th>No OF Rooms</th>
+                <th>Total Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {
+                bookedInfo.map(info=><MyBookingsTable info={info}></MyBookingsTable>)
+              }
+            </tbody>
+          </table>
         </div>
+      </div>
     );
 };
 
---
+++ client/src/Pages/MyBookings/MyBookingsTable.jsx
@@ -0,0 +1,28 @@
+import { CiEdit } from "react-icons/ci"
+import { MdCancel } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const MyBookingsTable = ({ info }) => {
+  return (
+    <tr>
+      <td>
+        <h1 className="font-bold text-xl">{info.category}</h1>
+      </td>
+      <td>{info.email}</td>
+      <td>{info.checkIn}</td>
+      <td>{info.checkOut}</td>
+      <td>{info.noOfRooms}</td>
+      <td>{info.totalPrice}</td>
+      <td className="flex text-xl space-x-3">
+        <Link>
+           <CiEdit></CiEdit>
+        </Link>
+        <Link>
+           <MdCancel></MdCancel>
+        </Link>
+      </td>
+    </tr>
+  );
+};
+
+export default MyBookingsTable;
